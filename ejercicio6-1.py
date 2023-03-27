97a769380aaf5dacdc2ca9e8af87d2244b968659
Julio Branda <kolstomier@protonmail.com>
2023-03-26 21:05:56 -0300

    ejercico6-1 corregido

--- ejercicio6-1.py
+++ ejercicio6-1.py
@@ -1,18 +1,29 @@
 class Vehiculo():
-    color = "color"
-    ruedas = 4
-    puertas = 4
+
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
 
 
 class Coche(Vehiculo):
-    velocidad = 0
-    cilindrada = 1500
+    def __init__(self, color, ruedas, puertas, velocidad_max, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self.velocidad_max = velocidad_max
+        self.cilindrada = cilindrada
+    def __str__(self):
+        return "color {}, {}km/h, {} ruedas, {} puertas, {}cc".format(self.color, self.velocidad_max, self.ruedas, self.puertas, self.cilindrada)
 
-  
-coche = Coche()
+class Camion(Vehiculo):
+    def __init__(self, color, ruedas, puertas, carga_max, tipo_de_acoplado):
+        super().__init__(color, ruedas, puertas)
+        self.carga_maxima = carga_max
+        self.tipo_de_acoplado = tipo_de_acoplado
+    def __str__(self):
+        return ("color {}, {} ruedas, {} puertas, {}kg, acoplado tipo {}".format(self.color, self.ruedas, self.puertas, self.carga_maxima, self.tipo_de_acoplado))
 
-print(coche.color)
-print(coche.ruedas)
-print(coche.puertas)
-print(coche.velocidad)
-print(coche.cilindrada)
+coche = Coche("Rojo", 4, 2, 180, 2100)
+camion = Camion("Naranja", 22, 2, 30000, "Playo")
+
+print(coche)
+print(camion)
